6e8885618bc2bf4c465d80a7e66a7108839bc3f5
luxorAqp <ggarcialuis86@gmail.com>
2020-07-04 14:19:06 -0500

    arrays avanzados
    recorrer arrays

--- 19-arrays.js
+++ 19-arrays.js
@@ -1,5 +1,8 @@
 'use strict'
 alert("ARRAYS");
+
+/*
+var elemento = parseInt(prompt("Que elemento del array quieres",0));
 // UN ARREGLO ES UNA COLECCION DE MUCHOS DATOS, 
 
 var arreglo = ["Luis","Garcia","Arequipa",true, 33];
@@ -10,3 +13,25 @@ var lenguajes = new Array("PHP","Java","C","Cobol");
 
 console.log(arreglo[4]);
 console.log(lenguajes[1]);
+
+console.log(typeof elemento);
+
+console.log(lenguajes[elemento]);
+
+*/
+
+var arreglo = new Array(0,1,2,3,4,5,6);
+
+var elemento =  parseInt(prompt("Ingrese la posición del arreglo"));
+
+if(elemento>=arreglo.length)
+{
+	alert("El arreglo es más corto");
+	console.log("El tamaño es mayor");
+
+}
+else
+{
+	//elemento =  parseInt(prompt("Ingrese la posición del arreglo"));
+	console.log(arreglo[elemento]);
+}
