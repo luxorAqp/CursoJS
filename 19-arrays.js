5ea418431e9c25bca6c6b07ece79cfa98abaa93d
luxorAqp <48073373+luxorAqp@users.noreply.github.com>
2020-07-04 00:03:39 -0500

    arrays
    inicio de arreglos

---
+++ 19-arrays.js
@@ -0,0 +1,12 @@
+'use strict'
+alert("ARRAYS");
+// UN ARREGLO ES UNA COLECCION DE MUCHOS DATOS, 
+
+var arreglo = ["Luis","Garcia","Arequipa",true, 33];
+
+// SE PUEDE DEFINIR UN ARRAY EN FORMA DE OBJETO
+
+var lenguajes = new Array("PHP","Java","C","Cobol");
+
+console.log(arreglo[4]);
+console.log(lenguajes[1]);
