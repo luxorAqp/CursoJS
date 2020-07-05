6d008656b5162739f516d71875b672bae1af2a49
luxorAqp <48073373+luxorAqp@users.noreply.github.com>
2020-07-04 19:30:26 -0500

    arreglos multidimensionales
    Crear arreglo dimensionales

---
+++ 20-arrays_multidimensionales.js
@@ -0,0 +1,12 @@
+'use strict'
+alert("MULDIMENTEIONAL");
+
+
+var categoria = ["comedia","terror","romance","ciencia ficcion"];
+
+var pelicula =  ["como si fuera la primera vez","SAW","PD TE AMO","Avengers"];
+
+var cine 	= [categoria,pelicula];
+
+console.log(cine[0][2]);
+console.log(cine[1][2]);
