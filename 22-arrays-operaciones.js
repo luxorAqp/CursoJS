c271823ae6211bd6ca9fd62c93e9a2f3a05ece10
luxorAqp <48073373+luxorAqp@users.noreply.github.com>
2020-07-07 20:42:12 -0500

    SORT-REVERGE
    FUNCIONES DE ARREGLOS

---
+++ 22-arrays-operaciones.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// CONVERTIR UN STRING EN UN ARREGLO
+
+var cadena = "texto1 texto2 texto3 texto4 texto5";
+
+var cadena_string = cadena.split(" ");
+
+console.log(cadena_string);
+
+// PARA ORDENAR UN ARREGLO ALFABETICAMENTE 
+
+var arreglo1 =[6,8,3,6,2,99,0];
+var arreglo2 =[6,8,3,6,2,99,0];
+var arreglo_ordenado = arreglo1.sort();
+var arreglo_invertido = arreglo2.reverse();
+console.log(arreglo_ordenado);
+
+console.log(arreglo_invertido);
