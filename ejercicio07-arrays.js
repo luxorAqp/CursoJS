2853b1d9d139985b24eb1f5dc6ab481876dc0ac2
luxorAqp <48073373+luxorAqp@users.noreply.github.com>
2020-07-11 15:40:52 -0500

    ejercicio2
    ejerciciso arrays

--- ejercicio07-arrays.js
+++ ejercicio07-arrays.js
@@ -1,11 +1,39 @@
 'use strict'
 alert("EJERCICIO ARRAYS");
 
+var CanNum = parseInt(prompt("Cuantos  valores tendra el ARRAY"));
+var Arreglo =[];
 
-for (var i = 0; i <= 6 ; i++)
+function LlenarArreglo(arreglo,CanDat)
 {
-	var arreglo = prompt("ingrese elemento del arreglo"+[i]);
-	console.log(arreglo);
+	for (var i = 0; i <= CanDat; i++) 
+	{
+		arreglo[i]= parseInt(prompt("Ingrese numero"));
+		
+	}
+
+	
+	return arreglo;
+
 }
 
-//console.log(arreglo);
+function MostrarArregloConsola(LlenarArreglo)
+{
+	console.log(LlenarArreglo);
+
+
+}
+function MostrarArregloPantalla(LlenarArreglo)
+{
+	document.write("<H1>" +"ARREGLO"+ "</H1>");
+
+
+}
+
+console.log();
+LlenarArreglo(Arreglo,CanNum);
+//MostrarArregloConsola(LlenarArreglo(Arreglo,CanNum));
+
+//MostrarArregloPantalla(LlenarArreglo(Arreglo,CanNum));
+
+
