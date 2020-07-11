3be6c32af760c99b4220fe92afc69928dc9f46c0
luxorAqp <48073373+luxorAqp@users.noreply.github.com>
2020-07-10 23:15:52 -0500

    ejercicio
    principio

---
+++ ejercicio07-arrays.js
@@ -0,0 +1,11 @@
+'use strict'
+alert("EJERCICIO ARRAYS");
+
+
+for (var i = 0; i <= 6 ; i++)
+{
+	var arreglo = prompt("ingrese elemento del arreglo"+[i]);
+	console.log(arreglo);
+}
+
+//console.log(arreglo);
