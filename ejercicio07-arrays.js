230bac9cbe045388378f4160576fd92350259411
luxorAqp <48073373+luxorAqp@users.noreply.github.com>
2020-07-12 20:12:49 -0500

    Ejercicio semiterminado
    falta buscar

--- ejercicio07-arrays.js
+++ ejercicio07-arrays.js
@@ -1,25 +1,81 @@
 'use strict'
 alert("EJERCICIO ARRAYS");
 
-var CanNum = parseInt(prompt("Cuantos  valores tendra el ARRAY"));
 var array =[];
 
-function LlenarArreglo(arreglo,CanDat)
+function FillArray()
 {
-	for (var i = 0; i < CanDat; i++) 
-	{
-		arreglo[i]= parseInt(prompt("Ingrese numero"));	
-		
-	}		
-}
+	var CountElements = prompt("Write the count of elements ");
 
-function ViewConsole(FunLleArr)
-{
-	for (var i = ; i >=  ; i   ) {
-		Things[i]
+	for (var i = 0; i < CountElements; i++) 
+	{
+		array[i] = prompt("Write element");
 	}
 
+	//ShowScreen();
+	//ReverseArray();
+	//ShowConsole();
+	//OrderArray();
+	SearchElement();
+
+
 }
 
+function ShowScreen()
+{
+	array.forEach(element =>
+	{
+		document.write("<H1>" + element + "</br>" +"</H1>");
+	})
+}
 
+function ShowConsole()
+{
+	array.forEach(function (element)
+		{
+			console.log("Elemento del array --> " +element);
+		});
+}
 
+function OrderArray()
+{
+	console.log("ARREGLO ORDENADO");
+	array.sort();
+	array.forEach(element =>
+	{
+		console.log(element);
+
+	});
+} 
+
+function ReverseArray()
+{
+	console.log("ARREGLO INVERTIDO");
+	array.reverse();
+	array.forEach(function (element)
+		{
+			console.log(element);
+		});
+}
+
+function SearchElement()
+{
+	var ElementSearch = prompt("Write the element to search");
+	array.forEach(element =>
+	{
+		if(ElementSearch == element)
+		{
+			console.log(ElementSearch);
+			console.log(element);
+			console.log("The element exits ");
+		}
+		else
+		{
+			console.log(ElementSearch);
+			console.log(element);
+			console.log("Elemente is null");
+		}
+
+	});
+}
+FillArray();
