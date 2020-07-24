48b1fe94f913a999b9b9e11a69801fb5cb7a54d5
luxorAqp <48073373+luxorAqp@users.noreply.github.com>
2020-07-23 21:12:21 -0500

    dom
    modificación de html

--- 24-DOM.js
+++ 24-DOM.js
@@ -1,2 +1,25 @@
 'use strict'
 alert("DOM");
+
+function cambiacolor(color1,color2)
+{
+	caja.style.background = color1;
+	caja.style.color = color2;
+
+}
+
+// METODO 1
+var caja = document.getElementById("micaja");
+
+caja.innerHTML = "¡TEXTO INGRESADO DESDE JS!";
+caja.style.background= "red";
+caja.style.color="white";
+caja.style.padding = "20px";
+
+//METODO 2
+
+var caja2 = document.querySelector("#micaja");
+caja2.style.background = "purple";
+caja2.style.color = "pink";
+//cambiacolor("green","black");
+console.log(caja);
